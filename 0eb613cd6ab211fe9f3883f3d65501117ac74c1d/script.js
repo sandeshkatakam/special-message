d45facc62ea14be006ebb7929de2044355b29acf
Sandesh Katkaam <sandeshkatakam@gmail.com>
2022-09-02 21:50:16 +0530

    h

--- 0eb613cd6ab211fe9f3883f3d65501117ac74c1d/script.js
+++ 0eb613cd6ab211fe9f3883f3d65501117ac74c1d/script.js
@@ -110,3 +110,16 @@ const questions = [
     ]
   }
 ]
+
+var images = ['image1.jpg', 'image2.jpg', 'image3.jpg'],
+    index  = 0,
+    $top   = $('#top-area');
+
+setInterval(function() {
+   $top.animate({ opacity: 0 }, 500, function() {
+     $top.css('background-image', 'url('+images[++index]+')');
+     $top.animate({ opacity: 1 }, 500, function() {
+       if(index === images.length) index = 0;
+     });
+   });
+}, 6000);
